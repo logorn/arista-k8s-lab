201633f9bc8ea4cdb3df2d0146ea7bbf939386a0
t.fouchard <t.fouchard@qwant.com>
2018-08-14 18:03:06 +0200

    packer qemu builders

--- packer-provisioner-build/build.sh
+++ packer-provisioner-build/build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+#PACKER_LOG=1 packer build \
 packer build \
-  -var-file=variables.json \
-  provisioner.json
+  -var-file=variables-vbox.json \
+  provisioner-vbox.json
 #  -on-error=ask \
-#  provisioner.json
+
+packer build \
+  -var-file=variables-qemu-qcow2.json \
+  provisioner-qemu.json
+
+packer build \
+  -var-file=variables-qemu-raw.json \
+  provisioner-qemu.json
