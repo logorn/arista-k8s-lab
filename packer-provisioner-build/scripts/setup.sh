4819fa8031ac5ef4d3992c97143a707ac1b6da22
t.fouchard <t.fouchard@qwant.com>
2018-08-28 12:17:05 +0200

    provisioner firsboot

--- packer-provisioner-build/scripts/setup.sh
+++ packer-provisioner-build/scripts/setup.sh
@@ -20,6 +20,47 @@ itf=$(ip link show | grep 'DOWN' | cut -d':' -f2)
 
 ip address add ${provisioner}/${mgmt_mask} dev ${itf}
 
+apt-get install policykit-1 -y
+
 echo "Installation completed..."
 
-su - vagrant -c "cd /provisioning && ansible-playbook ./provisioner.yml"
+cat <<'EOF' >/usr/local/bin/firstboot.sh
+#!/bin/bash
+(
+  flock -n 200 || exit 1
+  if [ ! -f ~/firstboot ]; then
+    cd /provisioning
+    ansible-playbook ./provisioner.yml
+    [ $? -eq 0 ] && touch ~/firstboot || exit 1
+  fi
+) 200>/var/lock/firstboot.lock
+EOF
+
+chmod 755 /usr/local/bin/firstboot.sh
+
+cat <<'EOF' >/etc/systemd/system/firstboot.service;
+[Unit]
+Description=first boot
+Wants=network-online.target
+After=network-online.target
+StartLimitInterval=200
+StartLimitBurst=5
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/firstboot.sh
+RestartSec=10
+Restart=on-failure
+StandardOutput=journal
+User=vagrant
+Group=vagrant
+
+[Install]
+#WantedBy=multi-user.target
+WantedBy=sysinit.target
+EOF
+
+systemctl daemon-reload
+systemctl enable firstboot
+
+#su - vagrant -c "cd /provisioning && ansible-playbook ./provisioner.yml"
